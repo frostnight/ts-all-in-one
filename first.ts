ee8d693980fc7b1e7a2237dd20ec7398e40b29e9
frostnight <frostnightsong@gmail.com>
2023-01-01 18:02:47 +0900

    소스 맛보기

--- first.ts
+++ first.ts
@@ -1,4 +1,20 @@
-const a: number = 5;
+const a: string = '5';
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: true = true;
+
 // function add(x: number, y: number): number { return x + y }
-// const add: (x: number, y: number) => number = (x, y) => x + y;
-// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 }
+// type Add = (x: number, y: number) => number;
+// const add: Add = (x, y) => x + y;
+
+// interface Add {
+//     (x: number, y: number): number;
+// }
+// const add: Add = (x, y) => x + y;
+
+const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 }
+
+const arr: Array<number> = [123, 456]
+const arr2: [number, number, string] = [123, 456, 'hello']
