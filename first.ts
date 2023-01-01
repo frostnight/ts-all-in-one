7509c0bff9db6aa2cff5623a139ab3e1c1727a4f
frostnight <frostnightsong@gmail.com>
2023-01-01 17:49:14 +0900

    설정 파일 및 소스코드

---
+++ first.ts
@@ -0,0 +1,4 @@
+const a: number = 5;
+// function add(x: number, y: number): number { return x + y }
+// const add: (x: number, y: number) => number = (x, y) => x + y;
+// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 }
